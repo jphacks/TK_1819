b143d7ee454baf6ff4d8d3cc70c922da49ea5cc5
akfk <37177664+akfk@user.noreply.github.com>
2018-10-28 00:49:08 +0900

    add sound player

--- raspberrypi/get.py
+++ raspberrypi/get.py
@@ -1,21 +1,28 @@
 import RPi.GPIO as GPIO
 import urllib.request
 import time
+import pygame
+
 url = 'https://hack-api.herokuapp.com/trashkan/1/status'
 req = urllib.request.Request(url)
 
-#led = 21
 led = [22, 10, 9, 11, 5, 6, 13, 2, 3, 4, 17, 27]
-#[2,3,4,17,27,22,10,9,11,5,6,13]
 
+pygame.init()
 
 GPIO.setmode(GPIO.BCM)
 for i in led:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, GPIO.LOW)
 
-def blink():
+
+def blink(soundnum):
     print("blink start")
+    if soundnum == 2:
+        sound = pygame.mixer.Sound("wav/piano1.wav")
+    else:
+        sound = pygame.mixer.Sound("wav/decision5.wav")
+    sound.play()
     for i in range(10):
         for j in led:
             GPIO.output(j, GPIO.HIGH)
@@ -28,8 +35,8 @@ try:
         with urllib.request.urlopen(req)as res:
             body = int(res.read())
             print(body)
-        if body == 1:
-            blink()
+        if body != 0:
+            blink(body)
         time.sleep(1)
 except urllib.error.URLError as err:
     print(err.reason)
