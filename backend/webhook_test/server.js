7ec6ae513521437297c2d732e09a2ed270f0ecc4
akfk <37177664+akfk@user.noreply.github.com>
2018-10-27 12:03:36 +0900

    add beacon sample

--- backend/webhook_test/server.js
+++ backend/webhook_test/server.js
@@ -1,4 +1,21 @@
 var lb = require('line-messaging');
 
 var app = require('express')();
-var bot = lb.Client
+let channelID_ENV = process.env.CHANNEL_ID;
+let channelSecret_ENV = process.env.CHANNEL_SECRET;
+let channelAccessToken_ENV = process.env.CHANNEL_ACCESSTOKEN;
+
+var bot = lb.Client({
+    channelID: channelID_ENV,
+    channelSecret: channelSecret_ENV,
+    channelAccessToken: channelAccessToken_ENV
+});
+app.use(bot.webhook('/webhook'));
+bot.on(lb.Events.MESSAGE, function(replyToken, message){
+    bot.replyTextMessage(replyToken, 'hello world!').then(function (data) {
+        console.log("hoge");
+    }).catch(function(error){
+        
+    });
+});
+app.listen(443);
