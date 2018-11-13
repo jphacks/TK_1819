fd346f7d7627c35f8cb8a6501ca815ac044058a9
Shun Kinoshita <shunkin@west.sd.keio.ac.jp>
2018-11-14 01:33:27 +0900

    separate functions and routeing

--- api/linebot/services/Linebot.js
+++ api/linebot/services/Linebot.js
@@ -215,9 +215,7 @@ module.exports = {
       .populate(populate);
   },
 
-  // beaconManager
-  //
-/**
+  /**
    * Route and handle each events.
    *
    * @return {resolve}
@@ -225,160 +223,184 @@ module.exports = {
 
   handleEvent: (event) => {
     if(event.beacon){
-      if (event.beacon.type === 'enter'){
-        if (user_hash[event.source.userId] == undefined) {
-          user_hash[event.source.userId] = {}
-          user_hash[event.source.userId]["areaID"] = "";
-          user_hash[event.source.userId]["level"] = 0;
-        }
-        user_hash[event.source.userId]["areaID"] = event.beacon.hwid;
-
-        client.pushMessage(event.source.userId, [ {
-          type: 'template',
-          altText: 'スマートフォンから確認してください',
-          template: {
-            type: 'buttons',
-            title: 'お知らせ', // 40文字以内
-            text: '近くに燃えるゴミ用のゴミ箱があります。ゴミはゴミ箱へ捨てましょう！捨てに行きますか？', // 60文字以内
-            thumbnailImageUrl: img_url, // httpsのみ可
-            actions: [{
-              type: 'message',
-              label: 'はい',
-              text: 'はい'
-            }, {
-              type: 'message',
-              label: 'いいえ',
-              text: 'いいえ'
-            }]
-          }
-        }, {
-          type: 'location',
-          title: 'ここにあります。',
-          address: '東京大学本郷キャンパス工学部２号館',
-          latitude: 35.7144598,
-          longitude: 139.7620094
-        }]
-        );
-      } else if (event.beacon.type === 'leave'){
-        user_hash[event.source.userId]["areaID"] = "";
-        // Exiting from zone
-        client.pushMessage(event.source.userId, [{
-          "text" : 'ばいばい',
-          "type" : 'text'
-        }]);
-      }
+      beaconHandler(event)
     }
 
     if (event.message.type != undefined && event.message.type == 'image') {
-      let image_buf;
-      const options = {
-        url: `https://api.line.me/v2/bot/message/${event.message.id}/content`,
-        method: 'get',
-        headers: {
-          'Authorization': 'Bearer ' + config.channelAccessToken,
-        },
-        encoding: null
-      };
-      const send_options = {
-        host: 'api.line.me',
-        path: `/v2/bot/message/${event.message.id}/content`,
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-          "Authorization": " Bearer " + config.channelAccessToken
-        },
-        method:'GET'
-      };
-
-      console.log("image message has been sent");
-      var req = http.request(send_options, function(res){
-        var data = [];
-
-        console.log("recieved the image");
-        res.on('data', function(chunk){
-          data.push(new Buffer(chunk));
-        }).on('error', function(err){
-          console.log(err);
-        }).on('end', function(){
-          console.log("finished recieving the image");
-          getObjectName(data);
-        });
-      });
-      // request(options, function(error, response, body) {
-      //   if (!error && response.statusCode == 200) {
-      //     // image_buf = body
-      //     image_buf = new Buffer(body)
-      //     console.log('file recieved');
-      //     getObjectName(image_buf.toString('binary'));
-      //   } else {
-      //     console.log(error);
-      //     process.exit(1);
-      //   }
-      // });
-
+      imageHandler(event)
     }
 
     if (event.type !== 'message' || event.message.type !== 'text') {
       return Promise.resolve(null);
     }
 
-
-    if (event.message.text === 'いっぱい' || event.message.text === 'まだ大丈夫' || event.message.text === 'ポイントは？') {
-      // level++;
-      client.pushMessage(event.source.userId, [{
-        "text" : 'ありがとうございます！',
-        "type" : 'text'
-      },{
-        "text" : 'あなたは'+ user_hash[event.source.userId]["level"] + 'pointあります。',
-        "type" : 'text'
-      }]
-      );
-
-      setTimeout(myFunc, 3000);
-
-    } else if (event.message.text === 'はい') {
-      can_flag = "1";
-      let cur_user_level = user_hash[event.source.userId]["level"];
-      if (cur_user_level > 5) {
-        user_hash[event.source.userId]["level"] = 0;
-      } else {
-        user_hash[event.source.userId]["level"] = cur_user_level + 1;
-      }
-      client.pushMessage(event.source.userId, [{
-        "type": "template",
-        "altText": "ゴミ箱はいっぱいでしたか？",
-        "template": {
-          "type": "confirm",
-          "text": "ゴミ箱はいっぱいでしたか？",
-          "actions": [
-            {
-              "type": "message",
-              "label": "いっぱい",
-              "text": "いっぱい"
-            },
-            {
-              "type": "message",
-              "label": "まだ大丈夫",
-              "text": "まだ大丈夫"
-            }
-          ]
-        }
-      }]
-      );
-
-      can_flag = "1";
-      setTimeout(myFunc, 3000);
-    } else {
-      client.replyMessage(event.replyToken, {
-        "text": event.message.text ,
-        "type" : 'text'
-      }
-      )
-        .catch((err) => {
-          if (err instanceof HTTPError) {
-            console.error("Request ot Line server went wrong status code:" + err.statusCode);
-          }
-        });
-    }
+    chatHandler(event)
+    
     return 0
   }
 };
+
+// helper functions
+/**
+ * Handle beacon events.
+ *
+ * @return {resolve}
+ */
+
+beaconHandler = (event) => {
+  if (event.beacon.type === 'enter'){
+    if (user_hash[event.source.userId] == undefined) {
+      user_hash[event.source.userId] = {}
+      user_hash[event.source.userId]["areaID"] = "";
+      user_hash[event.source.userId]["level"] = 0;
+    }
+    user_hash[event.source.userId]["areaID"] = event.beacon.hwid;
+
+    client.pushMessage(event.source.userId, [ {
+      type: 'template',
+      altText: 'スマートフォンから確認してください',
+      template: {
+        type: 'buttons',
+        title: 'お知らせ', // 40文字以内
+        text: '近くに燃えるゴミ用のゴミ箱があります。ゴミはゴミ箱へ捨てましょう！捨てに行きますか？', // 60文字以内
+        thumbnailImageUrl: img_url, // httpsのみ可
+        actions: [{
+          type: 'message',
+          label: 'はい',
+          text: 'はい'
+        }, {
+          type: 'message',
+          label: 'いいえ',
+          text: 'いいえ'
+        }]
+      }
+    }, {
+      type: 'location',
+      title: 'ここにあります。',
+      address: '東京大学本郷キャンパス工学部２号館',
+      latitude: 35.7144598,
+      longitude: 139.7620094
+    }]
+    );
+  } else if (event.beacon.type === 'leave'){
+    user_hash[event.source.userId]["areaID"] = "";
+    // Exiting from zone
+    client.pushMessage(event.source.userId, [{
+      "text" : 'ばいばい',
+      "type" : 'text'
+    }]);
+  }
+}
+
+/**
+ * Handle beacon events.
+ *
+ * @return {resolve}
+ */
+
+imageHandler = (event) => {
+  let image_buf;
+  const options = {
+    url: `https://api.line.me/v2/bot/message/${event.message.id}/content`,
+    method: 'get',
+    headers: {
+      'Authorization': 'Bearer ' + config.channelAccessToken,
+    },
+    encoding: null
+  };
+  const send_options = {
+    host: 'api.line.me',
+    path: `/v2/bot/message/${event.message.id}/content`,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      "Authorization": " Bearer " + config.channelAccessToken
+    },
+    method:'GET'
+  };
+
+  console.log("image message has been sent");
+  var req = http.request(send_options, function(res){
+    var data = [];
+
+    console.log("recieved the image");
+    res.on('data', function(chunk){
+      data.push(new Buffer(chunk));
+    }).on('error', function(err){
+      console.log(err);
+    }).on('end', function(){
+      console.log("finished recieving the image");
+      getObjectName(data);
+    });
+  });
+  // request(options, function(error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     // image_buf = body
+  //     image_buf = new Buffer(body)
+  //     console.log('file recieved');
+  //     getObjectName(image_buf.toString('binary'));
+  //   } else {
+  //     console.log(error);
+  //     process.exit(1);
+  //   }
+  // });
+}
+
+chatHandler = (event) => {
+  if (event.message.text === 'いっぱい' || event.message.text === 'まだ大丈夫' || event.message.text === 'ポイントは？') {
+    // level++;
+    client.pushMessage(event.source.userId, [{
+      "text" : 'ありがとうございます！',
+      "type" : 'text'
+    },{
+      "text" : 'あなたは'+ user_hash[event.source.userId]["level"] + 'pointあります。',
+      "type" : 'text'
+    }]
+    );
+
+    setTimeout(myFunc, 3000);
+
+  } else if (event.message.text === 'はい') {
+    can_flag = "1";
+    let cur_user_level = user_hash[event.source.userId]["level"];
+    if (cur_user_level > 5) {
+      user_hash[event.source.userId]["level"] = 0;
+    } else {
+      user_hash[event.source.userId]["level"] = cur_user_level + 1;
+    }
+    client.pushMessage(event.source.userId, [{
+      "type": "template",
+      "altText": "ゴミ箱はいっぱいでしたか？",
+      "template": {
+        "type": "confirm",
+        "text": "ゴミ箱はいっぱいでしたか？",
+        "actions": [
+          {
+            "type": "message",
+            "label": "いっぱい",
+            "text": "いっぱい"
+          },
+          {
+            "type": "message",
+            "label": "まだ大丈夫",
+            "text": "まだ大丈夫"
+          }
+        ]
+      }
+    }]
+    );
+
+    can_flag = "1";
+    setTimeout(myFunc, 3000);
+  } else {
+    client.replyMessage(event.replyToken, {
+      "text": event.message.text ,
+      "type" : 'text'
+    }
+    )
+      .catch((err) => {
+        if (err instanceof HTTPError) {
+          console.error("Request ot Line server went wrong status code:" + err.statusCode);
+        }
+      });
+  }
+}
