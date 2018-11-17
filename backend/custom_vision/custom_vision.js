41667c4b399336971276abc5e0d21ed22bcbaefc
tomtomtomtomtomtomtom <tom.america426@gmail.com>
2018-11-18 06:54:20 +0900

    add custom vision

--- backend/custom_vision/custom_vision.js
+++ backend/custom_vision/custom_vision.js
@@ -7,7 +7,7 @@ var formData = {
 
 
 var customVisionApiRequestOptions = {
-    uri: process.env.CUSTOM_VISION_API_URI,
+    uri: "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/09776bb1-e376-4557-b2c1-49fc7700eeef/image?iterationId=6554a808-deca-4481-b833-e6f7895b58ed",
     headers: {
         "Content-Type": "application/json",
         "Prediction-Key": "1e6c252eef53454ab399198a722d7a6d"
@@ -17,19 +17,19 @@ var customVisionApiRequestOptions = {
 
 request.post(customVisionApiRequestOptions, function (error, response, body) {
     // 結果取得OKの場合
+    
     if (!error && response.statusCode == 200) {
-
         // food タグ および カテゴリーを取得
-        if (response.body.predictions[0].tagName != "garbagebox") {
-            if (response.body.predictions[0].probability > 0.5) {
+        if (JSON.parse(response.body).predictions[0].tagName != "garbagebox") {
+            if (JSON.parse(response.body).predictions[0].probability > 0.5) {
                 tag = "Negative";
             }
         } else {
-            if (response.body.predictions[1].probability > 0.5) {
+            if (JSON.parse(response.body).predictions[1].probability > 0.5) {
                 tag = "garbagebox";
             }
         }
-
+        console.log(tag)
         // 取得したタグに対応してメッセージをセット
         // メッセージ送信
 
