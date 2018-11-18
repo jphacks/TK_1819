86bf424c74468a617bdabbe7f6ed0e6176787f0b
Shun Kinoshita <shunkin@west.sd.keio.ac.jp>
2018-11-18 09:36:29 +0900

    WIP

--- api/linebot/services/Linebot.js
+++ api/linebot/services/Linebot.js
@@ -1,3 +1,6 @@
+
+
+
 'use strict';
 
 const line = require('@line/bot-sdk');
@@ -451,15 +454,53 @@ const imageHandler = async (event) => {
   // let files = {}
   // files.images["File"] = image_buf 
   console.log(image_buf)
-  let files = image_buf 
-  files["path"] = '/tmp/tmp'
-  if (strapi.plugins.upload && Object.keys(files).length > 0) {
-    // Upload new files and attach them to this entity.
-    await strapi.plugins.upload.services.upload.uploadToEntity({
-      id: "5bf03a6b0c8ee12e4a869fce",
-      model: 'trashcan', 
-    }, files, null);
-  }
+  // let files = image_buf 
+  // files["path"] = '/tmp/tmp'
+  // if (strapi.plugins.upload && Object.keys(files).length > 0) {
+  //   // Upload new files and attach them to this entity.
+  //   await strapi.plugins.upload.services.upload.uploadToEntity({
+  //     id: "5bf03a6b0c8ee12e4a869fce",
+  //     model: 'trashcan', 
+  //   }, files, null);
+  // }
+
+  var formData = {
+        file: image_buf,
+  };
+
+  var customVisionApiRequestOptions = {
+    uri: "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/09776bb1-e376-4557-b2c1-49fc7700eeef/image?iterationId=6554a808-deca-4481-b833-e6f7895b58ed",
+    headers: {
+      "Content-Type": "application/json",
+      "Prediction-Key": "1e6c252eef53454ab399198a722d7a6d"
+    },
+    formData: formData
+  };
+
+  request.post(customVisionApiRequestOptions, function (error, response, body) {
+    // 結果取得OKの場合
+
+    if (!error && response.statusCode == 200) {
+      // food タグ および カテゴリーを取得
+      if (JSON.parse(response.body).predictions[0].tagName != "garbagebox") {
+        if (JSON.parse(response.body).predictions[0].probability > 0.5) {
+          tag = "Negative";
+        }
+      } else {
+        if (JSON.parse(response.body).predictions[1].probability > 0.5) {
+          tag = "garbagebox";
+        }
+      }
+      console.log(tag)
+      // 取得したタグに対応してメッセージをセット
+      // メッセージ送信
+
+      // 結果取得NGの場合
+    } else {
+      console.log("error: " + error);
+    }
+  });
+
 }
 
 const addScore = (currentScore, scoreNum) => {
